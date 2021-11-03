f284facd6f819070af6bba341c316ec1e3e00629
bennadon <78505357+bennadon@users.noreply.github.com>
2021-11-03 18:34:18 -0400

    cc

--- app.py
+++ app.py
@@ -34,7 +34,7 @@ def index():
 
     return response
 
-if __name__ == "__main__":
-    app.debug = False
-    port = int(os.environ.get("PORT", 33507))
-    
+# if __name__ == "__main__":
+#     app.debug = False
+#     port = int(os.environ.get("PORT", 33507))
+#

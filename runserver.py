d8b4de9c8703f47e03da476196dc2c4ec56d36a9
bennadon <78505357+bennadon@users.noreply.github.com>
2021-11-03 17:03:24 -0400

    efef

---
+++ runserver.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python
+
+#-----------------------------------------------------------------------
+# runserver.py
+# Author: Benjamin Nadon
+#-----------------------------------------------------------------------
+
+from sys import exit, stderr
+import argparse
+from reg import app
+
+def make_parser():
+    parser = argparse.ArgumentParser(description =
+    'Server for the registrar application',
+        allow_abbrev = False)
+    parser.add_argument("port", type = int, help =
+    "the port at which the server should listen")
+
+    return parser
+
+def main():
+    try:
+        parser = make_parser()
+        args = parser.parse_args()
+        port = args.port
+
+    except Exception as ex:
+        print(ex, file=stderr)
+        exit(2)
+
+
+    try:
+        app.run(host='0.0.0.0', port=port, debug=True)
+    except Exception as ex:
+        print(ex, file=stderr)
+        exit(1)
+
+if __name__ == '__main__':
+    main()

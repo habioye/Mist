21e476ff082503a2feb268b045349b2b38852f37
bennadon <78505357+bennadon@users.noreply.github.com>
2021-11-03 23:20:47 -0400

    Update index.js

--- index.js
+++ index.js
@@ -11,11 +11,18 @@ function initMap() {
   const marker = new google.maps.Marker({
    position: { lat: 40.34657, lng: -74.65631 },
    map: map,
+   // title: "title",
  });
  marker.addListener("click", () => {
       infoWindow.close();
       infoWindow.setContent(marker.getTitle());
       infoWindow.open(marker.getMap(), marker);
     });
-
 }
+
+// function addpoint(lat, long, text){
+//   const marker = new google.maps.Marker({
+//    position: { lat: lat, lng: long },
+//    map: map,
+//    title: text,
+// }

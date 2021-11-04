5c54b46bde52e8cecc1ed755419e0a3cf599dd27
bennadon <78505357+bennadon@users.noreply.github.com>
2021-11-04 10:28:23 -0400

    Update app.py

--- app/app.py
+++ app/app.py
@@ -9,7 +9,6 @@ from sys import stderr
 from flask import Flask, request, make_response
 from flask import render_template
 from app import mistdb, templates
-import map_query, add_event_proto
 
 #-----------------------------------------------------------------------
 
@@ -22,12 +21,12 @@ def index():
     long = request.args.get('long')
     lat = request.args.get('lat')
     text = request.args.get('text')
-    package = map_query("00:00:00-05:00", "23:59:59-05:00")
+    package = mistdb.map_query("00:00:00-05:00", "23:59:59-05:00")
     if(package[0] == False):
         print("error")
 
     if long is not None:
-        add_event_proto(long, lat, text)
+        mistdb.add_event_proto(long, lat, text)
 
     html = render_template("testmap.html")
 

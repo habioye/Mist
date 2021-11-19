04430d270380cf0f2fd69cc767e4926f4ac5c460
habioye <78454514+habioye@users.noreply.github.com>
2021-11-19 15:51:54 -0500

    Update app.py

--- app/app.py
+++ app/app.py
@@ -176,6 +176,57 @@ def friendscreen():
     response = make_response(html)
     return response
 
+def calstringmaker(month, year):
+
+     currcal = mistcalender.mistCalender(month,year)
+     daycount = currcal.monthlength
+     firstday = currcal.get_first_day()
+     firstday = firstday % 7
+     firstday = firstday + 1
+    
+     caldata = currcal.to_dict()
+     calstring = " <table class=\"table table-bordered table-hover\">"
+     calstring += "<tr style=\"background-color:black;color:white;\">"
+     calstring += "<th colspan=\"7\"><h3 align=\"center\">"
+     calstring += month
+     calstring += " "
+     calstring += year
+     calstring += "</h3></th>"
+     calstring += "</tr>"
+     calstring += "<tr style=\"background-color: rgb(110, 110, 110);\">"
+     calstring += "<th>Su</th>"
+     calstring += "<th>Mo</th>"
+     calstring += "<th>Tu</th>"
+     calstring += "<th>We</th>"
+     calstring += "<th>Th</th>"
+     calstring += "<th>Fr</th>"
+     calstring += "<th>Sa</th>"
+     calstring += "</tr>"
+     calstring += ""
+     calstring += ""
+     calstring += ""
+     currcount = -1 * firstday
+     currcount = currcount + 2
+     weekcount = 0
+     while currcount <= daycount:
+         if weekcount == 0:
+            calstring += "<tr>"
+         calstring += "<th>"
+         if currcount > 0:
+            calstring += currcount
+         calstring += "</th>"
+         currcount+=1
+         if weekcount == 7:
+             calstring += "</tr>"
+             weekcount = 0
+     if weekcount != 0:
+        calstring += "</tr>"
+     calstring += "</table>"
+     
+     return calstring
+    
+        
+
 @app.route('/calendar', methods=['GET'])
 def calendar():
     # username = authenticate()
@@ -192,55 +243,9 @@ def calendar():
             data.push(details[1])
         else:
             print(details[1])
-
-    # month = request.args.get('month')
-    # year = request.args.get('year')
-    # currcal = mistcalender.mistCalender(month,year)
-    # daycount = currcal.monthlength
-    # firstday = currcal.get_first_day()
-    # firstday = firstday % 7
-    # firstday = firstday + 1
-    #
-    #
-    #
-    # caldata = currcal.to_dict()
-    # calstring = " <table class=\"table table-bordered table-hover\">"
-    # calstring += "<tr style=\"background-color:black;color:white;\">"
-    # calstring += "<th colspan=\"7\"><h3 align=\"center\">"
-    # calstring += month
-    # calstring += " "
-    # calstring += year
-    # calstring += "</h3></th>"
-    # calstring += "</tr>"
-    # calstring += "<tr style=\"background-color: rgb(110, 110, 110);\">"
-    # calstring += "<th>Su</th>"
-    # calstring += "<th>Mo</th>"
-    # calstring += "<th>Tu</th>"
-    # calstring += "<th>We</th>"
-    # calstring += "<th>Th</th>"
-    # calstring += "<th>Fr</th>"
-    # calstring += "<th>Sa</th>"
-    # calstring += "</tr>"
-    # calstring += ""
-    # calstring += ""
-    # calstring += ""
-    # currcount = -1 * firstday
-    # currcount = currcount + 2
-    # weekcount = 0
-    # while currcount <= daycount:
-    #
-    #     if weekcount == 0:
-    #         calstring += "<tr>"
-    #     calstring += "<th>"
-    #     if currcount > 0:
-    #     calstring += currcount
-    #     calstring += "</th>"
-    #     currcount+=1
-    #     if weekcount == 7:
-    #         calstring += "</tr>"
-    #         weekcount = 0
-    #  
-
+    #month = request.args.get('month')
+    #year = request.args.get('year')
+    #calstring = calstringmaker(month, year)
 
     html = render_template("calendar.html", eventData = data)
     response = make_response(html)

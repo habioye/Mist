c2063905dee1f4f63f179b609355a9a3931224c7
habioye <78454514+habioye@users.noreply.github.com>
2021-11-28 16:53:22 -0500

    Update app.py
    here
    
    Update app.py
    
    Update calendar.html
    
    Update calendar.html
    
    Revert "Update app.py"
    
    This reverts commit b7394901c4666656385f3b9a6ff67166670b93b3.
    
    Revert "Update calendar.html"
    
    This reverts commit 3b4bcbca5df61433d21ba4ccec41904b09a637d4.

--- app/app.py
+++ app/app.py
@@ -176,6 +176,57 @@ def friendscreen():
     response = make_response(html)
     return response
 
+
+def calstringmaker(month, year):
+
+     currcal = mistcalender.mistCalender(month,year)
+     daycount = currcal.monthlength
+     firstday = currcal.get_first_day()
+     firstday = firstday % 7
+     firstday = firstday + 1
+    
+     caldata = currcal.to_dict()
+     calstring = " <table class=\"table table-bordered table-hover\">"
+     calstring += "<tr style=\"background-color:black;color:white;\">"
+     calstring += "<th colspan=\"7\"><h3 align=\"center\">"
+     calstring += month
+     calstring += " "
+     calstring += year
+     calstring += "</h3></th>"
+     calstring += "</tr>"
+     calstring += "<tr style=\"background-color: rgb(110, 110, 110);\">"
+     calstring += "<th>Su</th>"
+     calstring += "<th>Mo</th>"
+     calstring += "<th>Tu</th>"
+     calstring += "<th>We</th>"
+     calstring += "<th>Th</th>"
+     calstring += "<th>Fr</th>"
+     calstring += "<th>Sa</th>"
+     calstring += "</tr>"
+     calstring += ""
+     calstring += ""
+     calstring += ""
+     currcount = -1 * firstday
+     currcount = currcount + 2
+     weekcount = 0
+     while currcount <= daycount:
+         if weekcount == 0:
+            calstring += "<tr>"
+         calstring += "<th>"
+         if currcount > 0:
+            calstring += currcount
+         calstring += "</th>"
+         currcount+=1
+         if weekcount == 7:
+             calstring += "</tr>"
+             weekcount = 0
+     if weekcount != 0:
+        calstring += "</tr>"
+     calstring += "</table>"
+     
+     return calstring
+
+
 @app.route('/calendar', methods=['GET'])
 def calendar():
     # username = authenticate()
@@ -240,7 +291,9 @@ def calendar():
     #         calstring += "</tr>"
     #         weekcount = 0
     #
-    html = render_template("calendar.html", eventData = data)
+    calstring = calstringmaker(12,2021)
+    
+    html = render_template("calendar.html", eventData = data, calinfo = calstring)
     response = make_response(html)
     return response
 

a65efbc19408e8c9cc47a56624c2ae47e47a610a
habioye <78454514+habioye@users.noreply.github.com>
2021-11-29 05:07:53 -0500

    Update app.py

--- app/app.py
+++ app/app.py
@@ -186,8 +186,37 @@ def calstringmaker(month, year):
     firstday = currcal.get_first_day()
     firstday = firstday % 7
     firstday = firstday + 1
-    
-    calstring = " <table class=\"table table-bordered table-hover\">"
+    calstring = "<!DOCTYPE html>"
+    calstring += "<html>"
+    calstring += "<head>"
+    calstring += "</head>"
+    calstring += "<title>Mist - Calendar View</title>"
+    calstring += "<meta name=\"viewport\""
+    calstring += "content=\"width=device-width, initial-scale=1\">"
+    calstring += "<link rel=\"shortcut icon\" type=\"image/x-icon\" href=\"https://mist-asset.s3.us-east-2.amazonaws.com/temp+logo.ico\" />"
+    calstring += "<link rel=\"stylesheet\" href="
+    calstring += "https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css\">"
+    calstring += "</head>"
+    calstring += "<body>"
+    calstring += "<div>"
+    calstring += "<div class=\"col-9 grayborder\" id=\"eventinfo\"></div>"
+    calstring += "</div>"
+    calstring += " <div class =\"container-fluid\" style =\"background-color:rgb(255, 143, 0);\">"
+    calstring += "<div class=\"row\" style=\"background-color:rgb(255, 143, 0);\">"
+    calstring += "<div class = \"col-3\" display: inline-block; style = \"display:inline-block;padding:0;margin:0;\">"
+    calstring += "<a href = \"https://mist-princeton.herokuapp.com/\"><img src=\"https://mist-asset.s3.us-east-2.amazonaws.com/mist+map2.png\"  class=\"responsive\" style = \"max-width:30%;height:auto;float:left;\"></a>"
+    calstring += "</div>"
+    calstring += "<div class = \"col-6 align-middle\" display: inline-block;><center><h1 class=\"align-middle\" style=\"font-size:4vw;float:center;display:inline;\">Welcome to Mist!</h1></center></div>"
+    calstring += "<div class = \"col-3\"display: inline-block; style = \"display:inline-block;padding:0;\">"
+    calstring += "<a href = \"https://mist-princeton.herokuapp.com/calendar\"><img src=\"https://mist-asset.s3.us-east-2.amazonaws.com/mist+day.png\"  class=\"responsive\" style = \"max-width:30%;height:auto;float:right;\"></a>"
+    calstring += "</div>"
+    calstring += "</div>"
+    calstring += "</div>"
+    calstring += "<center><div class=\"header\">"
+    calstring += "<h2>Calendar View</h2>"
+    calstring += "<h3>Good <span id=\"ampmSpan\"></span> user.</h3>"
+    calstring += "</div></center>"        
+    calstring += " <table class=\"table table-bordered table-hover\">"
     calstring += "<tr style=\"background-color:black;color:white;\">"
     calstring += "<th colspan=\"7\"><h3 align=\"center\">"
     datetime_object = datetime.datetime.strptime(str(month), "%m")
@@ -227,6 +256,8 @@ def calstringmaker(month, year):
         if weekcount != 0:
             calstring += "</tr>"
     calstring += "</table>"
+    calstring += "</body>"
+    calstring += "</html>"
     return calstring
 
 
@@ -270,10 +301,10 @@ def calendar():
     #     calstring += "</tr>"
     # calstring += "</table>"
     
-    html = render_template("calendar.html", eventData = data, calinfo = calstring)
+    html = render_template(calstring)
     response = make_response(html)
-    response.set_cookie('monthq', 4)
-    response.set_cookie('yearq', 2021)
+    # response.set_cookie('monthq', 4)
+    # response.set_cookie('yearq', 2021)
     return response
 
 @app.route('/firsttimeuser', methods=['GET'])

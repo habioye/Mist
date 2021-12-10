2e7bc8aeee00c3afc8d57789161632228f0f7412
habioye <78454514+habioye@users.noreply.github.com>
2021-12-10 13:31:56 -0500

    Update app.py

--- app/app.py
+++ app/app.py
@@ -518,7 +518,7 @@ def eventstringmaker(day,month,year):
     date = dateformat(year, month, day)
     print('DATE')
     print(date)
-    events = mistdb.date_query(date, username)
+    events = mistdb.date_query(date,username)
     eventstring = ""
 
 
